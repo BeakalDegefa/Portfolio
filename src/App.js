ef175ce28f8e6f02b6bd5cb3feb46be754fdb120
Beakal Degefa <beakalpenn@gmail.com>
2023-03-17 13:27:55 -0400

    landing and about page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Link } from "react-scroll";
+import logo from "../src/images/logo.png"
+import about from "../src/images/photo_2023-03-14_13-28-48.jpg"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="be">
+        <p className="be">Logo</p>
+      </div>
+      <ul className="nav">
+        <li className="buttons">Home</li>
+        <li className="buttons"><Link to="about">About</Link></li>
+        <li className="buttons">Projects</li>
+        <li className="buttons">Contact</li>
+      </ul>
+      <div className="image">
+        <img className= "backgroundimg"src={logo}alt="logo"/>
+      </div>
+      <div className="text-container" >
+        <p className="text">I am Beakal Degefa.</p>
+        <p className="role">Web and Mobile Developer</p>
+      </div>
+      <div className="about-page">
+        <div className="buttons">About</div>
+        <div className="about-img">
+        <img className= "aboutimg"src={about}alt="beakal-pic"/>
+        </div>
+        <p className="aboutme"> This is Beakal intro Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo praesentium atque iure dolores tempore ab temporibus a neque placeat, incidunt alias ipsam est! Tenetur doloribus odio, eaque ipsum possimus quisquam. </p>
+        
+      </div>
     </div>
   );
 }
